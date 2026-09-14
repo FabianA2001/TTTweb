c936a3701de487d793ad3be0ec21695e9b185788
Fabian A <>
2026-09-14 18:05:58 +0200

    fix: allow request from localhost

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,8 +2,24 @@ import express from "express";
 import itemRoutes from "./routes/itemRoutes.ts";
 import tttRoutes from "./routes/tttRoutes.ts";
 import { errorHandler } from "./middlewares/errorHandler.ts";
-
+import cors from "cors";
 const app = express();
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (origin.startsWith("http://localhost:")) {
+        return callback(null, true);
+      }
+
+      callback(new Error("Nicht erlaubte Origin"));
+    },
+  }),
+);
 
 app.use(express.json());
 
